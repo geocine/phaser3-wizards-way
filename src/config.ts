0686df41563845586ba15ed26a5cd4933ac613e0
Aivan Monceller <aivan@monceller.com>
2023-02-25 12:09:13 +0000

    chore(init): initial wizard sprite

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@ import Phaser from 'phaser';
 export default {
   type: Phaser.AUTO,
   parent: 'game',
-  backgroundColor: '#33A5E7',
+  backgroundColor: '#bd9080',
   scale: {
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,64 @@
-import Phaser from 'phaser';
+import Phaser, { GameObjects } from 'phaser';
 
 export default class Demo extends Phaser.Scene {
+
+  wizardSprite?: Phaser.GameObjects.Sprite;
+
   constructor() {
     super('GameScene');
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.image('wizard', 'assets/wizard.png');
+  }
+
+  update() {
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+    let offset = 25;
+
+    this.wizardSprite = this.add.sprite(0, offset, 'wizard');
+    this.wizardSprite.flipX = true;
+    this.wizardSprite.setScale(0.25, 0.25);
+    // get the bounds of the wizardSprite
+    let bounds = this.wizardSprite.getBounds();
+    // center the wizardSprite
+    this.wizardSprite.setPosition(bounds.width / 2, offset + (bounds.height / 2));
+
+    // Create a graphics object to draw a rectangular box around the wizard sprite
+    let graphics = this.make.graphics();
+
+    // set the line style
+    graphics.lineStyle(2, 0x00ff00);
+
+    // draw a box around the wizard sprite
+    // graphics.strokeRect(
+    //   0,
+    //   28,
+    //   bounds.width,
+    //   bounds.height
+    // );
+
+    // set the line style
+    graphics.lineStyle(2, 0x00ff00);
+    let columns = 15
+    let rows = 8
+
+    for (let i = 0; i < columns; i++) {
+      for (let j = 0; j < rows; j++) {
+        graphics.strokeRect(
+          i + (i * bounds.width),
+          offset + j + (j * bounds.height),
+          bounds.width,
+          bounds.height
+        );
+      }
+    }
+
+    // add the graphics to the scene
+    this.add.existing(graphics);
+
   }
 }
